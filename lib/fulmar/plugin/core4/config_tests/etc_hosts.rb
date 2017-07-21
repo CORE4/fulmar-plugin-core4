c0b827966b8416f500d0e12291c2f3e5555046ef
Gerrit Visscher <g.visscher@core4.de>
2017-07-21 15:17:40 +0200

    Fix class name

--- lib/fulmar/plugin/core4/config_tests/etc_hosts.rb
+++ lib/fulmar/plugin/core4/config_tests/etc_hosts.rb
@@ -3,7 +3,7 @@ require 'rbconfig'
 global_test 'Find duplicates in hosts file' do |_config|
   is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
   hosts_file = is_windows ? 'C:\Windows\System32\drivers\etc\hosts' : '/etc/hosts'
-  duplicates = Fulmar::Plugin::CORE4::Hostfile.new(hosts_file).check
+  duplicates = Fulmar::Plugin::CORE4::HostFile.new(hosts_file).check
   next {severity: :warning,
     message: "Found duplicates in hosts file: #{duplicates.join(', ')}"
   } unless duplicates.empty?
